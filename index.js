ecb7302fb539bb0ab3d4dc784f4527f726e073a8
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-04-25 19:37:54 -0400

    both of the sides are working perfectly

--- index.js
+++ index.js
@@ -36,10 +36,7 @@ fetch
       <div id=edit-todo-${todo.id}>
       </div>`
     }
-    else {
-      todoContainer.innerHTML += ` `
-      pendingContainer.innerHTML += ` `
-    }
+
 
   })) 
 
@@ -52,14 +49,14 @@ fetch
 
   
     const contentInput = todoForm.querySelector('#content').value
-    const isCompleted_input = todoForm.querySelector('#isCompleated').value
+    
 
 
     fetch(`${todoURL}`, {
       method: 'POST',
       body: JSON.stringify({
         content: contentInput,
-        isCompleted: isCompleted_input
+        isCompleted: false
       }),
       
       headers: {
@@ -67,6 +64,7 @@ fetch
       }
     }).then( response => response.json())
       .then( todo => {
+        if( todo.isCompleted == 'true' || todo.isCompleted == true ) {
         alltodos.push(todo)
         todoContainer.innerHTML += `
         <div id=todo-${todo.id}>
@@ -76,7 +74,19 @@ fetch
         </div>
         <div id=edit-todo-${todo.id}>
         </div>`
-      })
+      }
+      else if( todo.isCompleted == 'false' || todo.isCompleted == false ) {
+        alltodos.push(todo)
+        pendingContainer.innerHTML += `
+        <div id=todo-${todo.id}>
+          <h2>${todo.content}</h2>
+          <button data-id=${todo.id} id="edit-${todo.id}" data-action="edit">Edit</button>
+          <button data-id=${todo.id} id="delete-${todo.id}" data-action="delete">Delete</button>
+        </div>
+        <div id=edit-todo-${todo.id}>
+        </div>`
+      }
+    })
 
   })
 
@@ -91,6 +101,72 @@ fetch
       })
 
       const editForm = todoContainer.querySelector(`#edit-todo-${e.target.dataset.id}`)
+      editForm.innerHTML = `
+        <form class='form' id='edit-todo' action='index.html' method='post'>
+          <form id="todo-form">
+            <input required id="edit-content" placeholder="${todoData.content}">
+            <input required id="edit-isCompleated" placeholder="${todoData.isCompleted}">
+            <input type="submit" value="Edit todo">
+        </form>`
+
+        editForm.addEventListener("submit", (e) => {
+          event.preventDefault()
+
+          const content_input = document.querySelector("#edit-content").value
+          const isCompleted_input = document.querySelector("#edit-isCompleated").value
+          const editedtodo = document.querySelector(`#todo-${todoData.id}`)
+
+          fetch(`${todoURL}/${todoData.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+              content: content_input,
+              isCompleted: isCompleted_input,
+
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then( response => response.json() )
+          .then( todo => {
+            editedtodo.innerHTML = `
+            <div id=todo-${todo.id}>
+              <h2>${todo.content}</h2>
+              <h4>isCompleated: ${todo.isCompleted}</h4>
+
+              <button data-id=${todo.id} id="edit-${todo.id}" data-action="edit">Edit</button>
+              <button data-id=${todo.id} id="delete-${todo.id}" data-action="delete">Delete</button>
+            </div>
+            <div id=edit-todo-${todo.id}>
+            </div>`
+            editForm.innerHTML = ""
+          })
+      })
+
+              
+    } else if (e.target.dataset.action === 'delete') {
+      document.querySelector(`#todo-${e.target.dataset.id}`).remove()
+        fetch(`${todoURL}/${e.target.dataset.id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then( response => response.json())
+      }
+
+  }) 
+
+
+  pendingContainer.addEventListener('click', (e) => {
+    if (e.target.dataset.action === 'edit') {
+
+      const editButton = document.querySelector(`#edit-${e.target.dataset.id}`)
+      editButton.disabled = true
+
+      const todoData = alltodos.find((todo) => {
+        return todo.id == e.target.dataset.id
+      })
+
+      const editForm = pendingContainer.querySelector(`#edit-todo-${e.target.dataset.id}`)
       editForm.innerHTML = `
         <form class='form' id='edit-todo' action='index.html' method='post'>
           <form id="todo-form">
