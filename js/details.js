4d0d231d18326eb5d6e626d3b6c2c986efb122ee
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-04-25 16:20:14 -0400

    delete button

--- js/details.js
+++ js/details.js
@@ -1,6 +1,7 @@
 // javascript for details.html
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.details');
+const deleteBtn = document.querySelector('.delete');
 
 
 const renderDetails =  async (e) => {
@@ -15,15 +16,17 @@ const renderDetails =  async (e) => {
         <p> Completed : ${post.isCompleted}</p>
     `
     container.innerHTML = template; 
-    // const item = e.target;
-    // console.log(item.classList[0]);
 
-    // const res = await fetch('http://localhost:3000/todos/' + id, { 
-    //     method: 'DELETE'
-    // });
 
-    // window.location.replace('/');
+
 };
 
+deleteBtn.addEventListener('click', async (e) => {
+    const res = await fetch('http://localhost:3000/todos/' + id, { 
+        method: 'DELETE'
+    });
+
+    window.location.replace('/');
+})
 
 window.addEventListener('DOMContentLoaded', renderDetails);
